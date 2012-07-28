393651c482cea79071ed341c1dc613e1c23d258d
qiwu <ivan198@gmail.com>
2012-07-28 17:16:35 +0800

    fix RIL and make clean

--- private/libhardware_legacy/wifi/wifi.c
+++ private/libhardware_legacy/wifi/wifi.c
@@ -48,6 +48,7 @@ extern char *dhcp_lasterror();
 extern void get_dhcp_info();
 extern int init_module(void *, unsigned long, const char *);
 extern int delete_module(const char *, unsigned int);
+extern void huawei_oem_rapi_streaming_function(int n, int p1, int p2, int p3, char *v1, int *v2, int *v3);
 
 static char iface[PROPERTY_VALUE_MAX];
 // TODO: use new ANDROID_SOCKET mechanism, once support for multiple
@@ -149,9 +150,6 @@ char* get_samsung_wifi_type()
     if (strncmp(buf, "semcove", 7) == 0)
         return "_semcove";
 
-    if (strncmp(buf, "semcosh", 7) == 0)
-        return "_semcosh";
-
     return NULL;
 }
 #endif
@@ -161,12 +159,26 @@ static int insmod(const char *filename, const char *args)
     void *module;
     unsigned int size;
     int ret;
-
+    char x[8];
+    int  y;
+    char mac_param[128];
+    char cust_mac_param[128];
     module = load_file(filename, &size);
     if (!module)
         return -1;
 
-    ret = init_module(module, size, args);
+	property_get("persist.sys.wifimac",cust_mac_param,"");
+	if(!strcmp(cust_mac_param,"")) {
+	        memset(x,0,8);
+	        y=0;
+	        huawei_oem_rapi_streaming_function(3,0,0,0,0,&y,x);
+	        LOGI("QIWU:huawei_oem_rapi_streaming_function %p %x %x",x,x[0],y);
+	        sprintf(mac_param,"mac_param=%02X:%02X:%02X:%02X:%02X:%02X %s",x[5],x[4],x[3],x[2],x[1],x[0],args);
+	} else {
+                sprintf(mac_param,"mac_param=%s %s",cust_mac_param,args);
+        }
+        LOGI("QIWU:Got MAC Address: %s ",mac_param);
+        ret = init_module(module, size, mac_param);
 
     free(module);
 
